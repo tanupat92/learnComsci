7c09e117c0e4c2e8fc53a68c16ccc34b270444f6
unknown <tanupat92@gmail.com>
2020-02-14 18:32:59 +0700

    data scraping

---
+++ datascrape/wk4beautifulsoup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 11:24:09 2020
+
+@author: tanupatsurface
+"""
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter -')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href',None))
